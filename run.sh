c9aab162e723eeb56e3d0b947355ede001c8459e
tryllerot <tryllerot@f0e6dd88-48a7-6224-061c-d0debb7c6e8b>
2011-07-30 22:43:16 +0000

    * Linux fixes * corrected hotkey message

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#The forgotten server run script
+#Crystal server run script
 
-echo "The forgotten server run script"
+echo "Crystal server run script"
 VERSION="1.00a"
 
 case "$1" in
@@ -10,16 +10,16 @@ case "$1" in
       echo >&2 "Usage $0 <options>"
       echo >&2 " Where options are:"
       echo >&2 " -h|--help|start|stop|restart|kill|--version";
-      if [ -f theforgottenserver ];
+      if [ -f crystalserver ];
       then
-        ./theforgottenserver --help
+        ./crystalserver --help
       fi
       ;;
    start|s)
-     if [ -f theforgottenserver ];
+     if [ -f crystalserver ];
      then
         echo >&2 "Running The forgotten server v$VERSION"
-        ./theforgottenserver
+        ./crystalserver
         echo >&2 "Exited with code $?"
       else
          echo >&2 "Could not start The forgotten server"
@@ -27,17 +27,17 @@ case "$1" in
      ;;
    restart)
      echo "Restarting..."
-     killall -9 theforgottenserver
-     if [ -f theforgottenserver ];
+     killall -9 crystalserver
+     if [ -f crystalserver ];
      then
-        ./theforgottenserver
+        ./crystalserver
 		echo >&2 "Exited with code $?"
      else
         echo >&2 "Failed"
      fi
      ;;
    stop|kill)
-     killall -9 theforgottenserver
+     killall -9 crystalserver
      ;;
    --version)
      echo "v$VERSION"

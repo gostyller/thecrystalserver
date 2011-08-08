2d84286b0b73cc87345ec90ec99a696814380a3e
tryllerot <tryllerot@f0e6dd88-48a7-6224-061c-d0debb7c6e8b>
2011-08-08 02:48:27 +0000

    *Added isSummon function. *Minor monster edit. *Added new destroy.lua script for destroying boxes, crates, etc like real tibia. *Finished sweet mangonaise elixir scripts (tested). [all updates are from TFS revs]

--- trunk/data/actions/scripts/foods/sweet_mangonaise_elixir.lua
+++ trunk/data/actions/scripts/foods/sweet_mangonaise_elixir.lua
@@ -1,28 +1,38 @@
 local config = {
-	amount = 10
+        amount = 10
 }
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local food = SPECIAL_FOODS[item.itemid]
-	local ring = getPlayerSlotItem(cid, CONST_SLOT_RING)
-	if(food == nil and not ring) then
-		return false
-	end
-	
-	if(getItemInfo(ring.itemid).showDuration) then
-		local capRequired, pFreeCap = (getItemInfo(ring.itemid).weight * config.amount), getPlayerFreeCap(cid)
+        local food = SPECIAL_FOODS[item.itemid]
+        local ring = getPlayerSlotItem(cid, CONST_SLOT_RING)
+        if(food == nil) then
+                return false
+        end
+       
+        if(not ring) then
+                doPlayerSendTextMessage(cid, MESSAGE_STATUS_SMALL, "You may want to equip a ring before eating this.")
+                doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
+                return false
+        end
+       
+        if(getItemInfo(ring.itemid).showDuration) then
+                local capRequired, pFreeCap = (getItemInfo(ring.itemid).weight * config.amount), getPlayerFreeCap(cid)
 
-		if(pFreeCap < capRequired) then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_SMALL, "You may want to free some capacity before doing this.")
-			doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
-			return false
-		end
-		
-		for i=1,config.amount do
-			doPlayerAddItemEx(cid, doCopyItem(ring).uid, true)
-		end
-	end
+                if(pFreeCap < capRequired) then
+                        doPlayerSendTextMessage(cid, MESSAGE_STATUS_SMALL, "You may want to free some capacity before doing this.")
+                        doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
+                        return false
+                end
+               
+                for i=1,config.amount do
+                        doPlayerAddItemEx(cid, doCopyItem(ring).uid, true)
+                end
+        else
+                doPlayerSendTextMessage(cid, MESSAGE_STATUS_SMALL, "You may want to equip the correct type of ring before eating this.")
+                doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF)
+                return false
+        end
 
-	doRemoveItem(item.uid, 1)
-	doCreatureSay(cid, food, TALKTYPE_MONSTER)
-	return true
+        doRemoveItem(item.uid, 1)
+        doCreatureSay(cid, food, TALKTYPE_MONSTER)
+        return true
 end
